3a31cae574720771ff38864cc5ae5e9a4b4463a0
Vladimir <tingaev@inbox.ru>
2023-12-23 12:36:54 +0400

    First commit

---
+++ hw1/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    printf("Let's\n");
+    printf("     go\n");
+    printf("       to walk\n");
+    printf("   *\n");
+    printf("  ***\n");
+    printf(" *****\n");
+    printf("*******\n");
+    printf(" HH HH\n");
+    printf(" ZZZZZ\n");
+    return 0;
+}
